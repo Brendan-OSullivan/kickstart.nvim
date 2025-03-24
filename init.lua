f86a8590436963f750aadc5d47d51336e20b1e04
Brendan <102007984+Brendan-OSullivan@users.noreply.github.com>
2025-03-24 21:28:12 +1100

    adding <leader>gf to get filepath

--- init.lua
+++ init.lua
@@ -184,6 +184,7 @@ vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagn
 -- or just use <C-\>C-n> to exit terminal mode
 vim.keymap.set('t', '<Esc><Esc>', '<C-\\><C-n>', { desc = 'Exit terminal mode' })
 
+vim.keymap.set('n', '<leader>gf', '<cmd>let @+=@%<CR>', { desc = '[G]et [F]ile path' })
 -- TIP: Disable arrow keys in normal mode
 vim.keymap.set('n', '<left>', '<cmd>echo "Use h to move!!"<CR>')
 vim.keymap.set('n', '<right>', '<cmd>echo "Use l to move!!"<CR>')
