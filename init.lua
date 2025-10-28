d5b9313ac260fdbddb26dbc2189df95faffc881c
Brendan-OSullivan <102007984+Brendan-OSullivan@users.noreply.github.com>
2025-10-28 15:13:01 +1100

    some minor changes

--- init.lua
+++ init.lua
@@ -91,7 +91,7 @@ vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
 
 -- Set to true if you have a Nerd Font installed and selected in the terminal
-vim.g.have_nerd_font = false
+vim.g.have_nerd_font = true
 
 -- [[ Setting options ]]
 -- See `:help vim.o`
@@ -185,6 +185,8 @@ vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagn
 vim.keymap.set('t', '<Esc><Esc>', '<C-\\><C-n>', { desc = 'Exit terminal mode' })
 
 vim.keymap.set('n', '<leader>gf', '<cmd>let @+=@%<CR>', { desc = '[G]et [F]ile path' })
+vim.keymap.set('n', '<leader>rl', '<cmd>LspRestart<CR>', { desc = '[R]estart [L]sp' })
+
 -- TIP: Disable arrow keys in normal mode
 vim.keymap.set('n', '<left>', '<cmd>echo "Use h to move!!"<CR>')
 vim.keymap.set('n', '<right>', '<cmd>echo "Use l to move!!"<CR>')
